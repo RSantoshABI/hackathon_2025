4c0737a574d401d89096b6cfd2d796fddbe7cb48
RSantoshABI <santosh.r@ab-inbev.com>
2025-09-22 14:24:36 +0000

    updated hyperparams

--- pricing/model.py
+++ pricing/model.py
@@ -75,7 +75,7 @@ class ElasticityModel:
 
     def _elasticity_constraint_loss(
         self,
-        penalty_weight: float = 50.0,
+        penalty_weight: float = 5000.0,
     ) -> tf.Tensor:
 
         own_elasticities = tf.linalg.diag_part(self.elasticity_matrix)
